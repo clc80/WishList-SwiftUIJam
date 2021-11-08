54c846ec17a4d181dac2ae897b48970e10fa40f2
Claudia Maciel <clc_80@yahoo.com>
2021-11-08 10:37:09 -0800

    Added Helper Files

---
+++ WishList/WishList/Helpers/CloseButton.swift
@@ -0,0 +1,25 @@
+//
+//  CloseButton.swift
+//  crochetStitches
+//
+//  Created by claudia maciel on 8/5/21.
+//
+
+import SwiftUI
+
+struct CloseButton: View {
+    var body: some View {
+        Image(systemName: "xmark")
+            .font(.system(size: 17, weight: .bold))
+            .foregroundColor(.white)
+            .padding(.all, 10)
+            .background(Color.black.opacity(0.6))
+            .clipShape(Circle())
+    }
+}
+
+struct CloseButton_Previews: PreviewProvider {
+    static var previews: some View {
+        CloseButton()
+    }
+}
